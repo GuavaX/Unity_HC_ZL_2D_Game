918b625906f8b4fdb59c89917beb0d1aa2a34994
GuavaX <57407388+GuavaX@users.noreply.github.com>
2019-11-12 21:31:44 +0800

    添加腳本20191112

---
+++ UnityProject/Assets/Script/First.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class First : MonoBehaviour
+{
+    public int Level;
+    public float WalkSpeed;
+    public string prop;
+    public bool mission;
+
+}
