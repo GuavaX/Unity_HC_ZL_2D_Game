0ef2352086429342847fa465cdba2c011db3f17a
GuavaX <57407388+GuavaX@users.noreply.github.com>
2019-11-14 21:40:16 +0800

    方法_20191114

---
+++ UnityProject/Assets/Script/LearnMethod.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LearnMethod : MonoBehaviour
+{
+    /// <summary>
+    /// 開車子
+    /// </summary>
+    private void Drive()
+    {
+        print("The car is driving");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Drive();
+        Drive();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
