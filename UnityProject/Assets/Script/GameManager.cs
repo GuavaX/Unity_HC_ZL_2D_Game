f885c2a7a53a756a6c4024d68e17ab278f8a736e
GuavaX <57407388+GuavaX@users.noreply.github.com>
2019-11-21 21:23:49 +0800

    生成Instantiate

--- UnityProject/Assets/Script/GameManager.cs
+++ UnityProject/Assets/Script/GameManager.cs
@@ -6,6 +6,22 @@ public class GameManager : MonoBehaviour
 {
     int ScoreNow;
     int HighestScore;
+    [Header("水管")]
+    public GameObject pipe;
+
+    /// <summary>
+    /// 生成水管
+    /// </summary>
+    public void BornPipe()
+    {
+        float y = Random.Range(-1.1f, 1.9f);
+
+        Vector3 pos = new Vector3(4.5f, y, 0);
+        Quaternion rot = new Quaternion(0, 0, 0, 0);
+
+        Object.Instantiate(pipe, pos, rot);
+
+    }
 
     public void AddScore()
     {
@@ -26,7 +42,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        BornPipe();
     }
 
     // Update is called once per frame
---
+++ UnityProject/Assets/Script/LearnAPI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LearnAPI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("輸出！");
+
+        Debug.Log("輸出!!!!!!");
+
+        print("隨機變數: " + Random.Range(50.5f, 100.5f));
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(Input.inputString);
+
+        if (Input.GetKeyDown("s"))
+        {
+            print("玩家按下了 s ");
+        }
+
+        
+    }
+}
